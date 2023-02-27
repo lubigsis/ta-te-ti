347dd14bb065db73c96aee08f6a9c82aa4051d36
lubigsis <lucre.baluc@gmail.com>
2023-02-27 00:37:31 -0300

    Hice 2 funciones para determinar aleatoriamente turnos de jugadores

--- main.js
+++ main.js
@@ -1,24 +1,48 @@
-//array bidimensional
+//array bidimensional. Acá se colocará c/u de las jugadas (3 filas y 3 columnas). Así se evita usar k' el DOM
+// p/obtener valores. Cada vez q' hay una partida o un turno, se imprime el resultado q' hay en ese Array
 const board = [
     ["", "", ""],
     ["", "", ""],
-    ["", "", ""],
+    ["", "", ""]
   ];
   
-  let turno = 0; //0 user, 1 = pc
-  const boardElement = document.querySelector("#boardJS");
-  const playerDiv = document.querySelector("#playerJS");
+  let turno = 0; //(0 user, 1 = pc)
+  let boardContenedor = document.querySelector("#boardJS");
+  let playerDiv = document.querySelector("#playerJS");
 
-  renderBoard();
-  /*--------------------------------------------------------------------------------------------------*/
-//recorro el array bidimensional
+  startGame();
+
+  
+
+
+  /*-----------------------------------------------------------------------------------TABLERO---------------*/
+//recorro el array bidimensional, primero los row y dsp las cell. (El método map regresa un arreglo)
   function renderBoard() {
-    const html = board.map((row) => {
-      const cells = row.map((cell) => {
+    let html = board.map((row) => {
+      let cells = row.map((cell) => {
         return `<button class="cell">${cell}</button>`;
       });
-      return `<div class="row">${cells.join("")}</div>`;
+      return `<div class="row">${cells.join("")}</div>`; //se imprime una capa p!acomodr c/u de las filas
     });
   
-    boardElement.innerHTML = html.join("");
+    boardContenedor.innerHTML = html.join(""); 
+  }
+
+  //----------------------------------------------------------------
+  function startGame() {
+    renderBoard();
+    turno = Math.random() < 0.5 ? 0 : 1; //el turno del jugador es aleatorio: (0 user, 1 = pc)
+    renderPlayer();
+  
+    if (turno === 0) {
+      playerPlays();
+    } else {
+      PCPlays();
+    }
+  }
+
+  //--------------------------------------------------------------Turnos jugadores
+  function renderPlayer() {
+    document.querySelector("#playerJS").textContent = 
+    `${turno === 0 ? "Turno usuario" : "Turno PC"}`;
   }
